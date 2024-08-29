def46e45b580b23602f9f2bff02357c67c509bc8
Phoenixwalter <107403585+phoenixwalter@users.noreply.github.com>
2024-08-29 16:17:36 +0630

    Update firebase-module.js

--- js/firebase-module.js
+++ js/firebase-module.js
@@ -299,8 +299,8 @@ document.getElementById("send-message").addEventListener('click', (e) => {
             });
             let chat = document.getElementById("message-container");
             chat.scrollTop = chat.scrollHeight;
-            setTimeout(() => document.getElementById("msg-input").value = '', 3000);
-            setTimeout(renderMessages, 3000);        
+            setTimeout(() => document.getElementById("msg-input").value = '', 1000);
+            setTimeout(renderMessages, 1000);        
         }
     }
     // fix = update the whole user object
